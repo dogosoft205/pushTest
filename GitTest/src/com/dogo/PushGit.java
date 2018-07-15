b88c3e758feb3f29f5aa2b0527a5f9c287f53c60
dogosoft205 <dokorean@gmail.com>
2018-07-15 17:14:00 +0900

    Push test

--- GitTest/src/com/dogo/PushGit.java
+++ GitTest/src/com/dogo/PushGit.java
@@ -1,10 +1,10 @@
+package com.dogo;
 
-public class MainClass {
+public class PushGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("¾È³ç!");
-		System.out.println("¾È³ç! 2");
 	}
 
 }
